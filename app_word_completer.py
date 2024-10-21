b1486aa94624c1eb1fe36f7cfbd7b3f04c750fa4
Viktor Uebelhart <154932840+viktoruebelhart@users.noreply.github.com>
2024-10-21 20:11:41 +0000

    commit app

--- app_word_completer.py
+++ app_word_completer.py
@@ -1,3 +1,24 @@
 import streamlit as st
+import gdown
+
+def load_model():
+     url = 'https://drive.google.com/ud?id=1L_vCM0lUTI3tejprp_O1KYUqPnUn-7e-'
+     gdown.download(url, 'model_seer_keras')
+     loaded_model = tf.keras.models.load_model('model_seer.keras')
+     with open ('vectorizer.pkl', 'rb') as file:
+          vectorizer = pickel.load(file)
+
+    return loaded_model, vectorizer
 
 def main():
+
+    max_sequence_len = 50
+
+    #load the model
+    loaded_model, vectorizer
+
+    st.title('Next Word Prediction')
+    input_text = st.text_input('Enter a text string:')
+
+if __name__ == "__main__":
+     main()
